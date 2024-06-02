ccdb76c4b60807d67766ba0cd5d8e53c65ec5f15
Razvan Nicolae <razvan.nicolae003@gmail.com>
2024-06-02 19:41:46 +0300

    first commit

---
+++ src/components/footer/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./footer.css";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+import PauseIcon from "@mui/icons-material/Pause";
+import ShuffleIcon from "@mui/icons-material/Shuffle";
+import RepeatIcon from "@mui/icons-material/Repeat";
+import SkipNextIcon from "@mui/icons-material/SkipNext";
+import SkipPreviousIcon from "@mui/icons-material/SkipPrevious";
+import PlaylistPlayIcon from "@mui/icons-material/PlaylistPlay";
+import VolumeDownIcon from "@mui/icons-material/VolumeDown";
+import Grid from "@mui/material/Grid"; // Grid version 1
+import Slider from "@mui/material/Slider";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <h1>Helo</h1>
+    </div>
+  );
+}
+
+export default Footer;
