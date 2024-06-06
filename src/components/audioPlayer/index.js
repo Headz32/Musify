0c40feafe9e5ac74df9a1b2542df1c5d6870e4cf
Razvan Nicolae <razvan.nicolae003@gmail.com>
2024-06-06 19:05:02 +0300

    token save change

--- src/components/audioPlayer/index.js
+++ src/components/audioPlayer/index.js
@@ -12,16 +12,13 @@ export default function AudioPlayer({
 }) {
   const [isPlaying, setIsPlaying] = useState(false);
   const [trackProgress, setTrackProgress] = useState(0);
-  var audioSrc = total[currentIndex]?.track.preview_url;
+  const audioSrc = total[currentIndex]?.track.preview_url;
 
   const audioRef = useRef(new Audio(total[0]?.track.preview_url));
-
-  const intervalRef = useRef();
-
+  const intervalRef = useRef(null);
   const isReady = useRef(false);
 
   const { duration } = audioRef.current;
-
   const currentPercentage = duration ? (trackProgress / duration) * 100 : 0;
 
   const handleNext = useCallback(() => {
@@ -29,6 +26,14 @@ export default function AudioPlayer({
       setCurrentIndex(currentIndex + 1);
     } else {
       setCurrentIndex(0);
+    }
+  }, [currentIndex, total.length, setCurrentIndex]);
+
+  const handlePrev = useCallback(() => {
+    if (currentIndex - 1 < 0) {
+      setCurrentIndex(total.length - 1);
+    } else {
+      setCurrentIndex(currentIndex - 1);
     }
   }, [currentIndex, total.length, setCurrentIndex]);
 
@@ -41,43 +46,41 @@ export default function AudioPlayer({
       } else {
         setTrackProgress(audioRef.current.currentTime);
       }
-    }, [1000]);
+    }, 1000);
   }, [handleNext]);
 
   useEffect(() => {
-    if (audioRef.current.src) {
-      if (isPlaying) {
-        audioRef.current.play();
-        startTimer();
-      } else {
-        clearInterval(intervalRef.current);
-        audioRef.current.pause();
-      }
+    if (isPlaying) {
+      audioRef.current.play().catch((error) => console.error(error));
+      startTimer();
     } else {
-      if (isPlaying) {
-        audioRef.current = new Audio(audioSrc);
-        audioRef.current.play();
-        startTimer();
-      } else {
-        clearInterval(intervalRef.current);
-        audioRef.current.pause();
-      }
+      clearInterval(intervalRef.current);
+      audioRef.current.pause();
     }
-  }, [isPlaying, audioSrc, startTimer]);
+  }, [isPlaying, startTimer]);
 
   useEffect(() => {
     audioRef.current.pause();
     audioRef.current = new Audio(audioSrc);
 
-    setTrackProgress(audioRef.current.currentTime);
+    const playAudio = () => {
+      audioRef.current.play().catch((error) => console.error(error));
+      setTrackProgress(audioRef.current.currentTime);
+      if (isReady.current) {
+        setIsPlaying(true);
+        startTimer();
+      } else {
+        isReady.current = true;
+      }
+    };
 
-    if (isReady.current) {
-      audioRef.current.play();
-      setIsPlaying(true);
-      startTimer();
-    } else {
-      isReady.current = true;
-    }
+    audioRef.current.addEventListener("loadeddata", playAudio);
+
+    return () => {
+      audioRef.current.removeEventListener("loadeddata", playAudio);
+      audioRef.current.pause();
+      clearInterval(intervalRef.current);
+    };
   }, [currentIndex, audioSrc, startTimer]);
 
   useEffect(() => {
@@ -87,22 +90,13 @@ export default function AudioPlayer({
     };
   }, []);
 
-  const handlePrev = useCallback(() => {
-    if (currentIndex - 1 < 0) {
-      setCurrentIndex(total.length - 1);
-    } else {
-      setCurrentIndex(currentIndex - 1);
-    }
-  }, [currentIndex, total.length, setCurrentIndex]);
-
   const addZero = (n) => {
     return n > 9 ? "" + n : "0" + n;
   };
 
-  const artists = [];
-  currentTrack?.album?.artists.forEach((artist) => {
-    artists.push(artist.name);
-  });
+  const artists = currentTrack?.album?.artists
+    .map((artist) => artist.name)
+    .join(" | ");
 
   return (
     <div className="player-body flex">
@@ -117,7 +111,7 @@ export default function AudioPlayer({
       </div>
       <div className="player-right-body flex">
         <p className="song-title">{currentTrack?.name}</p>
-        <p className="song-artist">{artists.join(" | ")}</p>
+        <p className="song-artist">{artists}</p>
         <div className="player-right-bottom flex">
           <div className="song-duration flex">
             <p className="duration">0:{addZero(Math.round(trackProgress))}</p>
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -6,71 +6,45 @@ import Trending from "../trending";
 import Feed from "../feed";
 import Favorites from "../favorites";
 import Sidebar from "../../components/sidebar";
-
-import "./home.css";
 import Login from "../auth/login";
 import { setClientToken } from "../../spotify";
 
+import "./home.css";
+
+const TOKEN_EXPIRY_TIME = 3600 * 1000; // 1 hour in milliseconds
+
 function Home() {
   const [token, setToken] = useState("");
-  const [expirationTime, setExpirationTime] = useState(null);
+
+  const setTokenInLocalStorage = (_token) => {
+    const expiryTime = Date.now() + TOKEN_EXPIRY_TIME;
+    window.localStorage.setItem("token", _token);
+    window.localStorage.setItem("token_expiry", expiryTime);
+    setClientToken(_token);
+  };
 
   useEffect(() => {
-    const storedToken = window.localStorage.getItem("token");
-    const storedExpirationTime = window.localStorage.getItem("expiration_time");
+    const token = window.localStorage.getItem("token");
+    const expiryTime = window.localStorage.getItem("token_expiry");
     const hash = window.location.hash;
     window.location.hash = "";
 
-    if (!storedToken && hash) {
-      const params = new URLSearchParams(hash.substring(1));
-      const _token = params.get("access_token");
-      const expiresIn = params.get("expires_in"); // Typically 3600 seconds
-      const _expirationTime = new Date().getTime() + expiresIn * 1000;
-
-      window.localStorage.setItem("token", _token);
-      window.localStorage.setItem("expiration_time", _expirationTime);
-
+    if (!token && hash) {
+      const _token = hash.split("&")[0].split("=")[1];
+      setTokenInLocalStorage(_token);
       setToken(_token);
-      setExpirationTime(_expirationTime);
-      setClientToken(_token);
-    } else if (storedToken && storedExpirationTime) {
-      const currentTime = new Date().getTime();
-      if (currentTime < storedExpirationTime) {
-        setToken(storedToken);
-        setExpirationTime(storedExpirationTime);
-        setClientToken(storedToken);
-      } else {
-        // Token has expired
+    } else if (token && expiryTime) {
+      if (Date.now() > expiryTime) {
+        // Token is expired
         window.localStorage.removeItem("token");
-        window.localStorage.removeItem("expiration_time");
+        window.localStorage.removeItem("token_expiry");
         setToken("");
-        setExpirationTime(null);
+      } else {
+        setToken(token);
+        setClientToken(token);
       }
     }
   }, []);
-
-  useEffect(() => {
-    if (expirationTime) {
-      const currentTime = new Date().getTime();
-      const timeout = expirationTime - currentTime;
-
-      if (timeout > 0) {
-        const timer = setTimeout(() => {
-          window.localStorage.removeItem("token");
-          window.localStorage.removeItem("expiration_time");
-          setToken("");
-          setExpirationTime(null);
-        }, timeout);
-
-        return () => clearTimeout(timer);
-      } else {
-        window.localStorage.removeItem("token");
-        window.localStorage.removeItem("expiration_time");
-        setToken("");
-        setExpirationTime(null);
-      }
-    }
-  }, [expirationTime]);
 
   return !token ? (
     <Login />
