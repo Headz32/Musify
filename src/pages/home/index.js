0bdd83fc7aab6f5d76060a9c4c3411135a4e9465
Razvan Nicolae <razvan.nicolae003@gmail.com>
2024-06-06 19:57:13 +0300

    minor changes

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -6,26 +6,43 @@ import Trending from "../trending";
 import Feed from "../feed";
 import Favorites from "../favorites";
 import Sidebar from "../../components/sidebar";
-
-import "./home.css";
 import Login from "../auth/login";
 import { setClientToken } from "../../spotify";
+
+import "./home.css";
+
+const TOKEN_EXPIRY_TIME = 3600 * 1000; // 1 hour in milliseconds
 
 function Home() {
   const [token, setToken] = useState("");
 
+  const setTokenInLocalStorage = (_token) => {
+    const expiryTime = Date.now() + TOKEN_EXPIRY_TIME;
+    window.localStorage.setItem("token", _token);
+    window.localStorage.setItem("token_expiry", expiryTime);
+    setClientToken(_token);
+  };
+
   useEffect(() => {
     const token = window.localStorage.getItem("token");
+    const expiryTime = window.localStorage.getItem("token_expiry");
     const hash = window.location.hash;
     window.location.hash = "";
+
     if (!token && hash) {
       const _token = hash.split("&")[0].split("=")[1];
-      window.localStorage.setItem("token", _token);
+      setTokenInLocalStorage(_token);
       setToken(_token);
-      setClientToken(_token);
-    } else {
-      setToken(token);
-      setClientToken(token);
+    } else if (token && expiryTime) {
+      if (Date.now() > expiryTime) {
+        // Token is expired
+        window.localStorage.removeItem("token");
+        window.localStorage.removeItem("token_expiry");
+        setToken("");
+      } else {
+        setToken(token);
+        setClientToken(token);
+      }
     }
   }, []);
 
