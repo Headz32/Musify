7cd0bed905534ffc0c6b3f3e8d7e284bbe05b9dd
Razvan Nicolae <razvan.nicolae003@gmail.com>
2024-06-07 08:54:55 +0300

    logout addon

--- src/pages/auth/logout.js
+++ src/pages/auth/logout.js
@@ -1,16 +1,13 @@
 // src/components/Logout.js
 import { useEffect } from "react";
+import Login from "./login";
 
 function Logout() {
   useEffect(() => {
     window.localStorage.removeItem("token");
-    window.localStorage.removeItem("expires_in");
-
-    // Refresh the page to trigger the token check and show the login page
-    window.location.reload();
   }, []);
 
-  return null;
+  return <Login />;
 }
 
 export default Logout;
